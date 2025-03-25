5a89c0936c8f4511a26c90c598922d0bc3ec8bad
kanelis <kanelisbulcina@gmail.com>
2025-03-25 22:31:25 +0200

    Maiņas

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -18,10 +18,9 @@
                 @enderror
 
             
-                <select name="category_name">
-                <option value="nav">Nav</option>
+                <select name="category_id">
                 @foreach ($categories as $category)
-                    <option value="{{ $category->category_name }}">{{ $category->category_name }}</option>
+                    <option value="{{ $category->id }}">{{ $category->category_name }}</option>
                 @endforeach
             </select>
             <button type="submit">Rediģēt ierakstu</button> 
